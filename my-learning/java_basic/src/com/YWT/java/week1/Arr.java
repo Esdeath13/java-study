3e7f67696d1a825fd16121cad1d9fd250a69626b
YWT <761081172@qq.com>
2020-10-05 19:52:59 +0800

    YWT

--- my-learning/java_basic/src/com/YWT/java/week1/Arr.java
+++ my-learning/java_basic/src/com/YWT/java/week1/Arr.java
@@ -1,8 +1,44 @@
-package com.YWT.java.week1;/**
-  *@ClassName Arr
-  *@Description TODO
-  *@Author YWT
-  *@Date 2020/10/5 16:41
-**/
+package com.YWT.java.week1;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * @ClassName Arr
+ * @Description TODO
+ * @Author YWT
+ * @Date 2020/10/5 16:41
+ **/
 public class Arr {
+    final static int MAX = 5;
+    public static void main(String[] args) {
+        int[] a= new int[MAX];
+        Random random = new Random();
+        for(int i = 0;i < MAX; i++){
+            a[i] = random.nextInt(100);
+            System.out.println(a[i] + " ");
+
+        }
+        int max = a[0];
+        for(int i = 0; i<a.length; i++){
+            if (a[i] > max){
+                max = a[i];
+            }
+        }
+        System.out.println("最大值为：" + max);
+        int min = a[0];
+        for(int i = 0; i<a.length; i++){
+            if(a[i] < min){
+                min = a[i];
+            }
+        }
+        System.out.println("最小值为：" + min);
+        int sum = a[0];
+        for (int i = 0; i<a.length; i++){
+            sum += a[i];
+        }
+        System.out.println("和为：" + sum);
+        Arrays.sort(a);
+        System.out.println("排序后：" + Arrays.toString(a));
+    }
 }
--- my-learning/java_basic/src/com/YWT/java/week1/Fac.java
+++ my-learning/java_basic/src/com/YWT/java/week1/Fac.java
@@ -1,8 +1,13 @@
-package com.YWT.java.week1;/**
-  *@ClassName Fac
-  *@Description TODO
-  *@Author YWT
-  *@Date 2020/10/5 16:29
-**/
+package com.YWT.java.week1;
+
+/**
+ * @ClassName Fac
+ * @Description TODO
+ * @Author YWT
+ * @Date 2020/10/5 16:29
+ **/
 public class Fac {
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
 }
